2e3b1d23610a566d6792ed6d7740658bc3727466
chaenabi <find1086@gmail.com>
2020-03-06 19:39:26 +0900

    DP chapter8 complete (Command Pattern)

--- src/chapter8/CommandPattern.java
+++ src/chapter8/CommandPattern.java
@@ -1,47 +1,84 @@
 package chapter8;
 
-public class CommandPattern {
-    Command command;
-    CommandPattern(Command command) {
-        this.command = command;
-    }
+// Command Pattern
 
-    public void setCommand(Command command) {
-        this.command = command;
-    }
+class Client {
+    public static void main(String[] args) {
+        Lamp lamp = new Lamp();
+        Command lampOnCommand = new LampOnCommand(lamp);
 
-    public void ado() {
-        command.execute();
+        Button lampButton = new Button(lampOnCommand); // 램프를 켜는 커맨드를 설정함
+        lampButton.pressed(); // 버튼이 눌리면, 램프를 켜는 기능이 실행됨.
+
+        Alarm alarm = new Alarm();
+        Command alarmOnCommand = new AlarmOnCommand(alarm);
+        Button alarmButton = new Button(alarmOnCommand);
+        alarmButton.pressed(); // 버튼이 눌리면, 알람을 울리는 기능이 실행됨.
+
+        alarmButton.setCommand(lampOnCommand); // 이 코드를 주석처리하면 버튼을 눌렀을 때마다
+                                                // 알람을 울리는 기능만 계속 실행된다.
+        alarmButton.pressed(); // 주석처리하지 않았으므로 램프를 켜는 기능이 실행된다.
+
+
     }
 }
+
+
 
 interface Command {
-    public void execute();
-
+    void execute();
 }
 
-class TurnClass implements Command {
-    Turn turn;
-    @Override
-    public void execute() {
-        turn.turnOn();
+class Button {
+    private Command theCommand;
+
+    public Button(Command theCommand) {
+        setCommand(theCommand);
+    }
+
+    public void setCommand(Command newCommand) {
+        this.theCommand = newCommand;
+    }
+
+    public void pressed() { // 버튼이 눌리면 주어진 Command의 execute 메서드를 호출함
+        theCommand.execute();
     }
 }
 
-class FlagClass implements Command {
-
-    @Override
-    public void execute() {
-
+class Alarm {
+    public void start() {
+        System.out.println("Alarming...");
     }
-
 }
 
-class Turn {
+class Lamp {
     public void turnOn() {
-
+        System.out.println("Lamp On");
     }
-    public void turnOff() {
+}
 
+class AlarmOnCommand implements Command { // 알람을 울리는 클래스
+    private Alarm theAlarm;
+
+    AlarmOnCommand(Alarm theAlarm) {
+        this.theAlarm = theAlarm;
+    }
+
+    @Override
+    public void execute() {
+        theAlarm.start();
+    }
+
+}
+class LampOnCommand implements Command { // 램프를 켜는 클래스
+    private Lamp theLamp;
+
+    LampOnCommand(Lamp theLamp) {
+        this.theLamp = theLamp;
+    }
+
+    @Override
+    public void execute() {
+        theLamp.turnOn();
     }
 }
